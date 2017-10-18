b4d2559555520ef1a9294b10ed58be5e060f3f7e
Daniel Phillips <danieledward.phillips@gmail.com>
2017-10-18 12:00:34 -0600

    last minute change to Env vars...

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -3,7 +3,7 @@ class NrelService
   def initialize(zipcode)
     @zipcode = zipcode
     @conn = Faraday.new("https://api.data.gov") do |faraday|
-      faraday.headers["X-Api-key"] = "iDTNTs2B4Ixz116i3QeZmu5FHLY0DfLEuqQdjEtd"
+      faraday.headers["X-Api-key"] = ENV["nrel_key"]
       faraday.adapter Faraday.default_adapter
     end
   end
